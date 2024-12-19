fc9c1bae89a28f718030951797fa24540ecd0a33
Craig Buchanan <craig.buchanan@cogniza.com>
2024-12-19 07:57:35 -0600

    adding verb parameter adding token parameter deprecating accesstoken and environment parameters

--- PsDossierOnDemand/Public/New-DossierObject.ps1
+++ PsDossierOnDemand/Public/New-DossierObject.ps1
@@ -2,32 +2,53 @@ function New-DossierObject {
 
     [CmdletBinding(SupportsShouldProcess)]
     param (
-        [Parameter(Mandatory)]
-        [string]$AccessToken,
+        [Parameter(Mandatory,ParameterSetName='Current')]
+        [object]$Token,
 
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory,ParameterSetName='Deprecated')]
         [ValidateSet('Staging','Production')]
         [string]$Environment,
+
+        [Parameter(Mandatory,ParameterSetName='Deprecated')]
+        [string]$AccessToken,
         
         [Parameter(Mandatory)]
         [string]$Path,
 
-        [Parameter(Mandatory,ValueFromPipeline)]
-        [object]$Data,
+        [string]$Verb = 'Create',
 
-        [string]$Verb = 'Create'
+        [string]$Query,
+
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [object]$Data
     )
     
     begin {
 
-        $Headers = @{
-            Authorization = "Bearer $AccessToken"
-            Accept = 'application/json'
+        switch ($PSCmdlet.ParameterSetName) {
+            'Current' {
+                $Headers = @{
+                    Authorization = "Bearer {0}" -f $Token.access_token
+                    Accept = 'application/json'
+                }
+        
+                $BaseId = "https://{0}d7.dossierondemand.com/api" -f ( $Token.environment -eq 'Staging' ? 'stage.' : '')
+                break
+            }
+            'Deprecated' {
+                $Headers = @{
+                    Authorization = "Bearer $AccessToken"
+                    Accept = 'application/json'
+                }
+        
+                $BaseId = "https://{0}d7.dossierondemand.com/api" -f ( $Environment -eq 'Staging' ? 'stage.' : '')
+                break
+            }
         }
 
-        $BaseId = "https://{0}d7.dossierondemand.com/api" -f ( $Environment -eq 'Staging' ? 'stage.' : '')
-        $Uri = '{0}/{1}/{2}' -f $BaseId, $Path, $Verb
+        $Uri = $Query ? '{0}/{1}/{2}?{3}' -f $BaseId, $Path, $Verb, $Query : '{0}/{1}/{2}' -f $BaseId, $Path, $Verb
         Write-Debug "Uri: $Uri"
+
     }
     
     process {
@@ -36,7 +57,7 @@ function New-DossierObject {
 
         $Body = $Data | ConvertTo-Json -Depth 5
 
-        if ($PSCmdlet.ShouldProcess($Path, "POST")) {
+        if ($PSCmdlet.ShouldProcess("$Path/$Verb", "POST")) {
 
             $Response = Invoke-WebRequest -Method Post -Uri $Uri -Headers $Headers -Body $Body -ContentType 'application/json' -Verbose:$false
 
