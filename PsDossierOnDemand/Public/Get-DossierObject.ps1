b94b3ecb1b2d039503f35b21157cdb695ea991a6
Craig Buchanan <craig.buchanan@cogniza.com>
2024-12-19 07:55:08 -0600

    adding help adding token parameter removing environment and accesstoken parameters adding expand parameter

--- PsDossierOnDemand/Public/Get-DossierObject.ps1
+++ PsDossierOnDemand/Public/Get-DossierObject.ps1
@@ -1,33 +1,63 @@
+<#
+.SYNOPSIS
+
+.PARAMETER Token
+Includes access token and environment.
+
+.PARAMETER Path
+.PARAMETER Id
+.PARAMETER Expand
+
+.EXAMPLE
+Get-DossierObject -Token $Token -Path 'Inventory/Parts' -ID 12345
+
+.EXAMPLE
+
+Get-DossierObject -Token $Token -Path 'Inventory/Parts' -ID 12345 -Expand ([Expand]::new('SystemCode'),[Expand]::new('AssemblyCode'),[Expand]::new('ComponentCode'))
+#>
 function Get-DossierObject {
 
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [string]$AccessToken,
-
-        [Parameter(Mandatory)]
-        [ValidateSet('Staging','Production')]
-        [string]$Environment,
+        [object]$Token,
         
         [Parameter(Mandatory)]
         [string]$Path,
 
-        [int]$Id
+        [int]$Id,
+
+        [Expand[]]$Expand
     )
     
     begin {
         $Headers = @{
-            Authorization = "Bearer $AccessToken"
+            Authorization = "Bearer {0}" -f $Token.access_token
             Accept = 'application/json'
-        }
-        $BaseId = "https://{0}d7.dossierondemand.com/api" -f ( $Environment -eq 'Staging' ? 'stage.' : '')
+        }        
+        $BaseId = "https://{0}d7.dossierondemand.com/api" -f ( $Token.environment -eq 'Staging' ? 'stage.' : '')
     }
     
     process {
 
         Write-Debug "Get-DossierObject($Path/$Id)"
 
-        $Uri = '{0}/{1}/{2}' -f $BaseId, $Path, $Id
+        # $Uri = if ($Operation) {
+        #     $Json = $Operation | ConvertTo-Json -Depth 10
+        #     Write-Debug "Json: $Json"
+        #     $Base64 = $Json | ConvertTo-Base64
+        #     '{0}/{1}/{2}?operation={3}' -f $BaseId, $Path, $Id, $Base64
+        # }
+        
+        $Uri = if ($Expand) {
+            $Json = @{ Expands = $Expand } | ConvertTo-Json -Depth 10
+            Write-Debug "Json: $Json"
+            $Base64 = $Json | ConvertTo-Base64
+            '{0}/{1}/{2}?operation={3}' -f $BaseId, $Path, $Id, $Base64
+        }
+        else {
+            '{0}/{1}/{2}' -f $BaseId, $Path, $Id
+        }
 
         $Response = Invoke-WebRequest -Method Get -Uri $Uri -Headers $Headers -Verbose:$false
 
